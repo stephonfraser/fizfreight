c4362f7c00e65e9e555bde8c91ffc8e0a964828a
Stephon Fraser <stephonfraserofficial@gmail.com>
2024-06-07 20:23:11 -0400

    Made changes to nav bar

--- app/workspace/components/NavigationBar.tsx
+++ app/workspace/components/NavigationBar.tsx
@@ -1,11 +1,84 @@
 import React from 'react'
+import Link from 'next/link'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 
 const NavigationBar = () => {
   return (
-    <div className='p-5 bg-gray-300 w-full'>
+    <div className='p-5 bg-gray-300 w-full flex justify-between items-center'>
+      <div className="home">
         Welcome to your dashboard
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar className='cursor-pointer hover:shadow-md '>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Billing
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Keyboard shortcuts
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>Email</DropdownMenuItem>
+                  <DropdownMenuItem>Message</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>More...</DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>GitHub</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuItem disabled>API</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <Link href={"/"}>
+            <DropdownMenuItem>
+              Log out
+            </DropdownMenuItem>
+          </Link>
+          
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      
+
     </div>
   )
 }
