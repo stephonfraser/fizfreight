a6a7c27b6be113b23ea122ee119c29bcc293a1bb
Stephon Fraser <stephonfraserofficial@gmail.com>
2024-06-09 19:45:41 -0400

    Added logic for adding new package

--- app/api/add-new-package/route.ts
+++ app/api/add-new-package/route.ts
@@ -3,16 +3,22 @@ import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const petName = searchParams.get('petName');
-  const ownerName = searchParams.get('ownerName');
+  const shipmentDate = searchParams.get('shipmentDate');
+  const deliveryDate = searchParams.get('deliveryDate');
+  const warehouseId = searchParams.get('warehouseId');
+  const customer = searchParams.get('customer');
+  const trackingNumber = searchParams.get('trackingNumber');
+  const weight = searchParams.get('weight');
+  const description = searchParams.get('description');
+  const vendor = searchParams.get('vendor');
  
   try {
-    if (!petName || !ownerName) throw new Error('Pet and owner names required');
-    await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+    if (!shipmentDate || !deliveryDate || !warehouseId || !customer || !trackingNumber || !weight || !description || !vendor) throw new Error('Pet and owner names required');
+    await sql`INSERT INTO packages (delivery_date, shipped_date, customer_id, warehouse_id, tracking_number, description, weight, vendor) VALUES (${deliveryDate}, ${shipmentDate}, ${customer}, ${warehouseId}, ${trackingNumber}, ${description}, ${weight}, ${vendor});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
  
-  const pets = await sql`SELECT * FROM Pets;`;
-  return NextResponse.json({ pets }, { status: 200 });
+  const packages = await sql`SELECT * FROM packages;`;
+  return NextResponse.json({ packages }, { status: 200 });
 }
